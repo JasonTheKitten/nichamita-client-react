eec35e0a23b2a87bbcd23306e34f81054832f5df
Jason Gronn <jasontopia03@gmail.com>
2024-06-24 01:58:39 -0400

    Reorganize to preferred setup

---
+++ src/app/chat/page.tsx
@@ -0,0 +1,9 @@
+import styles from './index.module.css';
+
+export default function Chat() {
+    return (
+        <div className={styles.container}>
+            <h1>Chat</h1>
+        </div>
+    );
+}
